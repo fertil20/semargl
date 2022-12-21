be3ff1478c1af4d11b91b2ef8b01619685400616
fertil <72992806+fertil20@users.noreply.github.com>
2022-12-21 03:54:32 +0300

    Шаблон

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import NotFound from "./common/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">Меню 1, Меню 2, Меню 3
+        </header>
+        <div className="container">
+            <Routes>
+                <Route path="/" element={<PersonList/>}/>
+                <Route errorElement={<NotFound/>}/>
+            </Routes>
+        </div>
     </div>
   );
 }
